97c4ecab24205609e0e2171bf333349e3c71623d
christian bonet <christianm.bonet@gmail.com>
2022-03-09 13:14:31 -0500

    cors edits

--- server/app.js
+++ server/app.js
@@ -13,6 +13,29 @@ connectDB();
 
 app.use(cors({ origin: true, credentials: true })); 
 
+var allowedOrigins = ['http://localhost:000','https://todaysreminders.herokuapp.com/'];
+ 
+app.use(cors({
+ 
+  origin: function(origin, callback){
+ 
+    if(!origin) return callback(null, true);
+ 
+    if(allowedOrigins.indexOf(origin) === -1){
+ 
+      var msg = 'The CORS policy for this site does not ' +
+ 
+                'allow access from the specified Origin.';
+ 
+      return callback(new Error(msg), false);
+ 
+    }
+ 
+    return callback(null, true);
+ 
+  }
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
