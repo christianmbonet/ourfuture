8f043289e51b21447041f52714bb2ce438ca0244
christian bonet <christianm.bonet@gmail.com>
2022-03-09 10:15:22 -0500

    changing file structure, moving routes and middleware set up to app.js in server folder

--- server.js
+++ server.js
@@ -1,34 +1,8 @@
-const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const app = require('./server/app');
+const port = process.env.PORT || 8000;
 
-
-const app = express();
-const connectDB = require("./server/config/db");
-
-const todo = require("./server/routes/todo");
-
-connectDB();
-
-app.use(cors({ origin: true, credentials: true })); 
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/todo", todo);
-
-app.get("/", (req, res) => res.send("Server up and running"));
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
-  }
-
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Express server is up on port ${port}`);
 });
-
---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+
+const app = express();
+const connectDB = require("./config/db");
+
+const todo = require("./routes/todo");
+
+connectDB();
+
+app.use(cors({ origin: true, credentials: true })); 
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/todo", todo);
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
+  module.exports = app;
